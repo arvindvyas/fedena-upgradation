e527f99678d477e4698eb1f6364295910d328b36
Arvind Vyas <arvindvyas07@gmail.com>
2017-03-07 00:35:54 +0530

    change company name

--- config/initializers/fedena_setting.rb
+++ config/initializers/fedena_setting.rb
@@ -1,6 +1,6 @@
 FEDENA_DEFAULTS = {
-  :company_name => 'Fedena',
-  :company_url  => 'http://www.projectfedena.org'
+  :company_name => 'Campus Management System',
+  :company_url  => 'http://www.arvindvyas.com'
 }
 
 USER_SETTINGS = {}
